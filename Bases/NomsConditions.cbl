610ca22609a0bee52f4a7d8f8a4e867187d40c6e
Wonder-Phoenix <138680353+Wonder-Phoenix@users.noreply.github.com>
2023-09-21 15:24:36 +0200

    *

--- Bases/NomsConditions.cbl
+++ Bases/NomsConditions.cbl
@@ -1,23 +1,23 @@
-#********************************************************************************************************************#
-#  ÉNONCÉ : Utilisation des noms conditions ; Utilisation de la fonction EVALUATE                                    #
-#           Écrire les variables suivantes en déclarant les noms conditions adéquats :                               #
-#               - le statut d'un compte ouvert s'il est égal à 'O', bloqué pour 'B', clôturé pour 'C' puis afficher  #
-#                 le statut ;                                                                                        #
-#               - le compte est créditeur si son solde est positif ou nul, débiteur s'il est négatif puis afficher   #
-#                 le sens créditeur ou débiteur                                                                      #
-#********************************************************************************************************************#
+      *********************************************************************************************************************#
+      *  ÉNONCÉ : Utilisation des noms conditions ; Utilisation de la fonction EVALUATE                                    #
+      *           Écrire les variables suivantes en déclarant les noms conditions adéquats :                               #
+      *               - le statut d'un compte ouvert s'il est égal à 'O', bloqué pour 'B', clôturé pour 'C' puis afficher  #
+      *                 le statut ;                                                                                        #
+      *               - le compte est créditeur si son solde est positif ou nul, débiteur s'il est négatif puis afficher   #
+      *                 le sens créditeur ou débiteur                                                                      #
+      *********************************************************************************************************************#
 
-#*******************************************************************#
-#                            PROGRAMME                              #
-#*******************************************************************#
+      ********************************************************************#
+      *                            PROGRAMME                              #
+      ********************************************************************#
 
 IDENTIFICATION DIVISION.
  PROGRAM-ID. EXO6.
  AUTHOR. SV.
  DATE-WRITTEN. 20/03/2023.
-******************************************************************
-*  FONCTION DU PROGRAMME :  EXERCICE SUR LES NOMS CONDITIONS     *
-******************************************************************
+      ******************************************************************
+      *  FONCTION DU PROGRAMME :  EXERCICE SUR LES NOMS CONDITIONS     *
+      ******************************************************************
  ENVIRONMENT DIVISION.
  CONFIGURATION SECTION.
  SPECIAL-NAMES.
@@ -26,13 +26,13 @@ IDENTIFICATION DIVISION.
  DATA DIVISION.
  
  WORKING-STORAGE SECTION.
-* VARIABLE POUR LE POSITIONNEMENT DU STATUT DU COMPTE
+      *VARIABLE POUR LE POSITIONNEMENT DU STATUT DU COMPTE
 01  STATUT           PIC X        VALUE SPACES.
     88 OUVERT                     VALUE 'O'.
     88 BLOQUE                     VALUE 'B'.
     88 CLOTURE                    VALUE 'C'.
 
-* VARIABLES POUR LE POSITIONNEMENT DU SENS DU SOLDE
+      *VARIABLES POUR LE POSITIONNEMENT DU SENS DU SOLDE
 01 SOLDE             PIC S9(9)V99 VALUE +2436,91.
 01                   PIC X        VALUE SPACES.
     88 CREDITEUR                  VALUE 'C'.
@@ -47,7 +47,7 @@ IDENTIFICATION DIVISION.
      
      ACCEPT STATUT
      
-* 1ERE FORME POSSIBLE
+      *1ERE FORME POSSIBLE
      EVALUATE STATUT
       WHEN 'O'
            DISPLAY 'COMPTE OUVERT'
@@ -59,7 +59,7 @@ IDENTIFICATION DIVISION.
            DISPLAY 'STATUT ERRONE'
      END-EVALUATE
      
-* 2EME FORME POSSIBLE
+      *2EME FORME POSSIBLE
      EVALUATE TRUE
       WHEN OUVERT
            DISPLAY 'COMPTE OUVERT'
